fd1d719450068d20b94f42e64e80ea7b74962ddf
Lucian Ghinea <lucian.ghinea@netcentric.biz>
2019-12-09 15:39:42 +0000

    Initial structure

---
+++ src/main/java/com/test/automation/testautomation/service/PageService.java
@@ -0,0 +1,4 @@
+package com.test.automation.testautomation.service;
+
+public class PageService {
+}
---
+++ src/main/java/com/test/automation/testautomation/validation/ValidationError.java
@@ -0,0 +1,4 @@
+package com.test.automation.testautomation.validation;
+
+public class ValidationError {
+}
